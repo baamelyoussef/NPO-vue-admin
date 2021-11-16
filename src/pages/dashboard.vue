4bbfaf000a02aaaf11ca9ba2d2639b65cb1635e4
Baamel youssef <49789574+baamelyoussef@users.noreply.github.com>
2021-11-16 14:31:59 +0100

    Sidebar changes

--- src/pages/dashboard.vue
+++ src/pages/dashboard.vue
@@ -8,37 +8,31 @@
     </v-app-bar>
     <v-navigation-drawer v-model="drawer"  app class="wall shadow">
       <v-card dark tile flat class="wall">
-        <v-list nav dense class="menu-links">
-          <div v-for="item in links" :key="item.title">
-            <v-list-group no-action v-if="item.items">
-              <template #activator>
-                <v-list-item-icon>
-                  <v-icon v-text="item.icon"></v-icon>
-                </v-list-item-icon>
-                <v-list-item-content>
-                  <v-list-item-title v-text="item.title"></v-list-item-title>
-                </v-list-item-content>
-              </template>
-              <v-list-item
-                v-for="child in item.items"
-                :key="child.title"
-                :to="`/${child.path}`"
-                router
-              >
-                <v-list-item-content>
-                  <v-list-item-title v-text="child.title"></v-list-item-title>
-                </v-list-item-content>
-              </v-list-item>
-            </v-list-group>
-            <v-list-item v-else class="alone-list" :to="`/${item.path}`" router>
-              <v-list-item-icon class="regular">
-                <v-icon>{{ item.icon }}</v-icon>
+        <v-img 
+                  lazy-src="https://i.postimg.cc/X7mxW0Sm/logonpo.png"
+                  max-height="50px"
+                  max-width="50px"
+                  class="ma-4"
+                  align="center"
+                  justify="center"
+                  src="https://i.postimg.cc/X7mxW0Sm/logonpo.png"></v-img>
+        <v-list dense>
+          <v-list-item-group
+            v-model="selectedItem"
+            color="primary"
+          >
+            <v-list-item
+              v-for="(item, i) in items"
+              :key="i"
+            >
+              <v-list-item-icon>
+                <v-icon v-text="item.icon"></v-icon>
               </v-list-item-icon>
               <v-list-item-content>
-                <v-list-item-title v-text="item.title"></v-list-item-title>
+                <v-list-item-title v-text="item.text"></v-list-item-title>
               </v-list-item-content>
             </v-list-item>
-          </div>
+          </v-list-item-group>
         </v-list>
       </v-card>
     </v-navigation-drawer>
@@ -53,25 +47,10 @@ export default {
   data() {
     return {
       drawer: true,
-      links: [
-        {
-          title: "Core HR",
-          icon: "mdi-calendar-multiple-check",
-          path: "#",
-          items: [
-            { title: "link-1", icon: "mdi-account-group", path: "jobs" },
-            {
-              title: "link-2",
-              icon: "mdi-account-group",
-              path: "employee",
-            },
-            {
-              title: "link-3",
-              icon: "mdi-account-group",
-              path: "position",
-            },
-          ],
-        },
+      items: [
+        { text: 'Organization info', icon: 'mdi-domain' },
+        { text: 'Engagement Info', icon: 'mdi-clipboard-check' },
+        { text: 'Nominated Personal', icon: 'mdi-account-tie' },
       ],
     };
   },
