7ac7e7f8d35f48b47cdea423d7f3d886c3e9e9c4
mohamed jamoun <mjamoun28@gmail.com>
2021-11-17 18:04:35 +0100

    </>

--- src/pages/organization-info.vue
+++ src/pages/organization-info.vue
@@ -168,31 +168,33 @@
               </v-row>
               <v-row>
                 <v-col cols="12" md="4">
-                  <p>Renewed 80G,12A,FCRA per the new guidelines?</p>
+                  <p class="mb-1 secondary--text">
+                    Renewed 80G,12A,FCRA per the new guidelines?
+                  </p>
                   <v-radio-group v-model="radioGroup" row>
                     <v-radio label="Yes" color="primary" value="Yes"></v-radio>
-                    <v-radio label="No" color="wall" value="No"></v-radio>
+                    <v-radio label="No" color="primary" value="No"></v-radio>
                   </v-radio-group>
                 </v-col>
 
                 <v-col cols="12" md="4">
-                  <p>
+                  <p class="mb-1 secondary--text">
                     Availability of Audited Financial statements for previous 3
                     years?
                   </p>
                   <v-radio-group v-model="radioGroup2" row>
                     <v-radio label="Yes" color="primary" value="Yes"></v-radio>
-                    <v-radio label="No" color="wall" value="No"></v-radio>
+                    <v-radio label="No" color="primary" value="No"></v-radio>
                   </v-radio-group>
                 </v-col>
                 <v-col cols="12" md="4">
-                  <p>
+                  <p class="mb-1 secondary--text">
                     Compliant with Income Tax Return filing for previous 3
                     years?
                   </p>
                   <v-radio-group v-model="radioGroup3" row>
                     <v-radio label="Yes" color="primary" value="Yes"></v-radio>
-                    <v-radio label="No" color="wall" value="No"></v-radio>
+                    <v-radio label="No" color="primary" value="No"></v-radio>
                   </v-radio-group>
                 </v-col>
               </v-row>
@@ -222,13 +224,13 @@
                 </v-col>
 
                 <v-col cols="12" md="4">
-                  <p>
+                  <p class="mb-1 secondary--text">
                     Any litigation history against the organization ? If yes,
                     please provide details
                   </p>
                   <v-radio-group v-model="radioGroup4" row>
                     <v-radio label="Yes" color="primary" value="Yes"></v-radio>
-                    <v-radio label="No" color="wall" value="No"></v-radio>
+                    <v-radio label="No" color="primary" value="No"></v-radio>
                   </v-radio-group>
                   <v-textarea
                     label="Details here"
@@ -236,7 +238,6 @@
                     outlined
                     rows="3"
                     row-height="25"
-                    shaped
                   ></v-textarea>
                 </v-col>
               </v-row>
@@ -266,92 +267,92 @@
             <v-form v-model="valid">
               <v-row>
                 <v-col cols="12" md="4">
-                  <p>Details of NPO Person filling the form :</p>
-                  <v-textarea
+                  <p class="mb-1 secondary--text">Details of NPO Person filling the form :</p>
+                  <v-text-field
                     class="mx-2"
                     label="Name"
-                    rows="1"
+                    outlined dense
                     prepend-icon="mdi-account-details"
-                  ></v-textarea>
-                  <v-textarea
+                  ></v-text-field>
+                  <v-text-field
                     class="mx-2"
                     label="Designation"
-                    rows="1"
+                    outlined dense
                     prepend-icon="mdi-information-outline"
-                  ></v-textarea>
-                  <v-textarea
+                  ></v-text-field>
+                  <v-text-field
                     class="mx-2"
                     label="E-mail"
-                    rows="1"
+                    outlined dense
                     prepend-icon="mdi-email"
-                  ></v-textarea>
-                  <v-textarea
+                  ></v-text-field>
+                  <v-text-field
                     class="mx-2"
                     label="Mobile"
-                    rows="1"
+                    outlined dense
                     prepend-icon="mdi-cellphone"
-                  ></v-textarea>
+                  ></v-text-field>
                 </v-col>
 
                 <v-col cols="12" md="4">
-                  <p>Details of NPO Champion :</p>
-                  <v-textarea
+                  <p class="mb-1 secondary--text">Details of NPO Champion :</p>
+                  <v-text-field
                     class="mx-2"
                     label="Name"
-                    rows="1"
+                    outlined dense
                     prepend-icon="mdi-account-details"
-                  ></v-textarea>
-                  <v-textarea
+                  ></v-text-field>
+                  <v-text-field
                     class="mx-2"
                     label="Designation"
-                    rows="1"
+                    outlined dense
                     prepend-icon="mdi-information-outline"
-                  ></v-textarea>
-                  <v-textarea
+                  ></v-text-field>
+                  <v-text-field
                     class="mx-2"
                     label="E-mail"
-                    rows="1"
+                    outlined dense
                     prepend-icon="mdi-email"
-                  ></v-textarea>
-                  <v-textarea
+                  ></v-text-field>
+                  <v-text-field
                     class="mx-2"
                     label="Mobile"
-                    rows="1"
+                    outlined dense
                     prepend-icon="mdi-cellphone"
-                  ></v-textarea>
+                  ></v-text-field>
                 </v-col>
 
                 <v-col cols="12" md="4">
-                  <p>Details of NPO CEO/ED/Funder</p>
-                  <v-textarea
+                  <p class="mb-1 secondary--text">Details of NPO CEO/ED/Funder</p>
+                  <v-text-field
                     class="mx-2"
                     label="Name"
-                    rows="1"
+                    outlined dense
                     prepend-icon="mdi-account-details"
-                  ></v-textarea>
-                  <v-textarea
+                  ></v-text-field>
+                  <v-text-field
                     class="mx-2"
                     label="Designation"
-                    rows="1"
+                    outlined dense
                     prepend-icon="mdi-information-outline"
-                  ></v-textarea>
-                  <v-textarea
+                  ></v-text-field>
+                  <v-text-field
                     class="mx-2"
                     label="E-mail"
-                    rows="1"
+                    outlined dense
                     prepend-icon="mdi-email"
-                  ></v-textarea>
-                  <v-textarea
+                  ></v-text-field>
+                  <v-text-field
                     class="mx-2"
                     label="Mobile"
-                    rows="1"
+                    outlined dense
                     prepend-icon="mdi-cellphone"
-                  ></v-textarea>
+                  ></v-text-field>
                 </v-col>
               </v-row>
               <v-row>
                 <v-col cols="12" md="4">
-                  <p>
+                  <p class="mb-1 secondary--text">
                     Explain organization's origin,purpose and core activities.
                     What is the social problem you are addressing
                   </p>
@@ -361,12 +362,11 @@
                     outlined
                     rows="3"
                     row-height="25"
-                    shaped
                   ></v-textarea>
                 </v-col>
 
                 <v-col cols="12" md="4">
-                  <p>
+                  <p class="mb-1 secondary--text">
                     Who are the direct and indirect beneficiaries of the
                     organisation
                   </p>
@@ -376,32 +376,31 @@
                     outlined
                     rows="3"
                     row-height="25"
-                    shaped
                   ></v-textarea>
                 </v-col>
 
                 <v-col cols="12" md="4">
-                  <p>What are the key qualitative impact indicators measured</p>
+                  <p class="mb-1 secondary--text">
+                    What are the key qualitative impact indicators measured
+                  </p>
                   <v-textarea
                     label="Explain here"
                     auto-grow
                     outlined
                     rows="3"
                     row-height="25"
-                    shaped
                   ></v-textarea>
                 </v-col>
               </v-row>
               <v-row>
                 <v-col cols="12" md="12">
-                  <p>Mention the SDG's impacted</p>
+                  <p class="mb-1 secondary--text">Mention the SDG's impacted</p>
                   <v-textarea
                     label="Mention here (if known)"
                     auto-grow
                     outlined
                     rows="3"
                     row-height="25"
-                    shaped
                   ></v-textarea>
                 </v-col>
               </v-row>
@@ -432,7 +431,7 @@
               <v-row>
                 <v-col cols="12" md="12">
                   <v-row align="center" justify="center">
-                    <p>
+                    <p class="mb-1 secondary--text">
                       Please provide supporting documents with the form
                       Organisational Funding details in Crors (Past and
                       Projected)
@@ -496,7 +495,7 @@
 
                 <v-col cols="12" md="12">
                   <v-row align="center" justify="center">
-                    <p>
+                    <p class="mb-1 secondary--text">
                       Please provide Committed and expected funding details in
                       croces
                     </p>
@@ -543,7 +542,7 @@
 
                 <v-col cols="12" md="12">
                   <v-row align="center" justify="center" class="mb-2">
-                    <p>
+                    <p class="mb-1 secondary--text">
                       Please provide Committed and expected funding details in
                       croces
                     </p>
@@ -592,7 +591,7 @@
               <v-row>
                 <v-col cols="12" md="12">
                   <v-row align="center" justify="center" class="mb-2">
-                    <p>
+                    <p class="mb-1 secondary--text">
                       Organisational Impact/ End beneficiary quantitative
                       details (Past and Projected)
                     </p>
@@ -627,7 +626,7 @@
 
                 <v-col cols="12" md="12">
                   <v-row align="center" justify="center" class="mb-2">
-                    <p>
+                    <p class="mb-1 secondary--text">
                       Provide names of states in wich you have project offices
                       in India
                     </p>
@@ -652,7 +651,7 @@
 
                 <v-col cols="12" md="12">
                   <v-row align="center" justify="center">
-                    <p>
+                    <p class="mb-1 secondary--text">
                       Organisational employee details (as of last month or Qtr)
                     </p>
                   </v-row>
@@ -707,7 +706,7 @@
               <v-row>
                 <v-col cols="12" md="12">
                   <v-row align="center" justify="center">
-                    <p>Organisational Risk Details</p>
+                    <p class="mb-1 secondary--text">Organisational Risk Details</p>
                   </v-row>
                   <v-simple-table>
                     <template v-slot:default>
@@ -743,7 +742,9 @@
 
                 <v-col cols="12" md="12">
                   <v-row align="center" justify="center">
-                    <p>Organisational IT infrastructure availability details</p>
+                    <p class="mb-1 secondary--text">
+                      Organisational IT infrastructure availability details
+                    </p>
                   </v-row>
                   <v-simple-table>
                     <template v-slot:default>
@@ -779,7 +780,7 @@
                 </v-btn>
               </div>
               <div class="d-flex justify-end mt-5">
-                <v-btn large rounded color="wall" class="white--text">
+                <v-btn large rounded color="primary" class="white--text">
                   Engagement Info
                   <v-icon right>mdi-arrow-right</v-icon>
                 </v-btn>
